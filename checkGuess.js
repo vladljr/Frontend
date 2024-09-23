87e9bd75a2314475802f3330b4279ac37aabb470
vladljr <164288366+vladljr@users.noreply.github.com>
2024-09-23 19:10:20 -0300

    Delete checkGuess.js

--- checkGuess.js
+++
@@ -1,9 +0,0 @@
-export function checkGuess(playerGuess, correctNumber) {
-    if (playerGuess > correctNumber) {
-        return "O número aleatório é menor que esse!";
-    } else if (playerGuess < correctNumber) {
-        return "O número aleatório é maior que esse!";
-    } else {
-        return "Parabéns! Você adivinhou o número!";
-    }
-}
